4337048cdf60f48e397c30cc9172d8a51032dff8
YazykovaDaria <yazykovadasva@gmail.com>
2021-12-11 17:50:58 +0300

    fix file

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
-export const welcomeUser = () => {
-    console.log('Welcome to the Brain Games!');
-    const UserName = readlineSync.question('May I have your name?: ');
-    console.log(`Hello, ${UserName}!`);
+const welcomeUser = () => {
+  console.log('Welcome to the Brain Games!');
+  const UserName = readlineSync.question('May I have your name?: ');
+  console.log(`Hello, ${UserName}!`);
 };
 
-
+export default welcomeUser;
