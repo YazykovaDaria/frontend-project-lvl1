3e4b69ba58cb2ac53c124cc9b00805baf3fa1b00
YazykovaDaria <yazykovadasva@gmail.com>
2021-12-19 22:10:35 +0300

    change random

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-export const randomNumber = () => {
-  const maxNumber = 100;
-  const minNumber = 1;
-  return Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
-};
+// eslint-disable-next-line max-len
+export const randomNumber = (maxNumber, minNumber) => Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
 
 export const question = 'Question: ';
 export const wrongAnswer = ' is wrong answer ;(. Correct answer was ';
