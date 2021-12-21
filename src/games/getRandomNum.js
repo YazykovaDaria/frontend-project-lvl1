492d5beb27f5f623a0762686b657ed28367fabf9
YazykovaDaria <yazykovadasva@gmail.com>
2021-12-21 19:22:58 +0300

    rename file

--- src/games/getRandomNum.js
+++
@@ -1,6 +0,0 @@
-const getRandomNumber = (maxNumber, minNumber) => {
-  const result = Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
-  return result;
-};
-
-export default getRandomNumber;
