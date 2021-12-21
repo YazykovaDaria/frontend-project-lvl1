7b2476c9d49b03b8e73b9933de9fa9c604f6a858
YazykovaDaria <yazykovadasva@gmail.com>
2021-12-21 22:48:37 +0300

    add file

---
+++ bin/brain-prime.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import * as game from '../src/games/brain-prime.js';
+import playGames from '../src/index.js';
+
+playGames(game);
---
+++ src/games/brain-prime.js
@@ -0,0 +1,19 @@
+import { getRandomNumber } from './utilite.js';
+
+export const rulesOfTheGame = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+export const generationTask = () => {
+  const task = getRandomNumber(1, 100);
+  return task;
+};
+
+export const correctAnswer = (task) => {
+  const searchAnsver = Number(task);
+  const stopSearch = Math.sqrt(searchAnsver);
+  for (let i = 2; i <= stopSearch; i += 1) {
+    if (searchAnsver % i === 0) {
+      return 'no';
+    }
+  }
+  return 'yes';
+};
