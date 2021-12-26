cad8dd397d1ca3f0c1c91e4aefa9bbcc64dfb73e
YazykovaDaria <yazykovadasva@gmail.com>
2021-12-26 22:52:54 +0300

    edit func

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -1,23 +1,25 @@
-import * as utilite from './utilite.js';
+import getRandomNumber from '../../utilite.js';
 
 export const rulesOfTheGame = 'Find the greatest common divisor of given numbers.';
-// понимаю что поиск правильного ответа надо как-то упрощать, но не могу додуматься как(
-export const correctAnswer = (string) => {
-  const reseveNumber = utilite.getArrayWithString(string);
-  let value1 = reseveNumber[0];
-  let value2 = reseveNumber[1];
-  while (value1 !== 0 && value2 !== 0) {
-    if (value1 > value2) {
-      value1 %= value2;
-    } else {
-      value2 %= value1;
-    }
-  }
-  return value1 + value2;
-};
 
 export const generationTask = () => {
-  const number1 = utilite.getRandomNumber(1, 100);
-  const number2 = utilite.getRandomNumber(1, 100);
+  const number1 = getRandomNumber(1, 100);
+  const number2 = getRandomNumber(1, 100);
   return `${number1} ${number2}`;
+};
+
+const gcd = (number1, number2) => {
+  if (!number2) {
+    return number1;
+  }
+
+  return gcd(number2, number1 % number2);
+};
+
+export const correctAnswer = (task) => {
+  const reseveNumber = task.split(' ');
+  const value1 = Number(reseveNumber[0]);
+  const value2 = Number(reseveNumber[1]);
+  const result = gcd(value1, value2);
+  return result;
 };
