32aac30aea11b4604bf7175211f5cdf7f05a6535
YazykovaDaria <yazykovadasva@gmail.com>
2022-01-10 17:53:14 +0300

    change  func

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -26,21 +26,22 @@ const calculate = (number1, number2, operator) => {
 };
 
 const generateRound = () => {
-  const gameRaunds = [];
-  for (let counter = 1; counter <= rounds; counter += 1) {
-    const number1 = getRandomNumber(1, 100);
-    const number2 = getRandomNumber(1, 100);
-    const operator = getRandomOperator();
-    const question = `${number1} ${operator} ${number2}`;
-    const correctAnswer = calculate(number1, number2, operator);
-    const round = [question, correctAnswer];
-    gameRaunds.push(round);
-  }
-  return gameRaunds;
+  const number1 = getRandomNumber(1, 100);
+  const number2 = getRandomNumber(1, 100);
+  const operator = getRandomOperator();
+  const question = `${number1} ${operator} ${number2}`;
+  const correctAnswer = calculate(number1, number2, operator);
+  const round = [question, correctAnswer];
+  // gameRounds.push(round);
+  return round;
 };
 
-const questionAnswer = generateRound();
-
-const brainCalc = () => playGames(rulesOfTheGame, questionAnswer);
-
+const brainCalc = () => {
+  const gameRounds = [];
+  for (let counter = 1; counter <= rounds; counter += 1) {
+    const round = generateRound();
+    gameRounds.push(round);
+  }
+  playGames(rulesOfTheGame, gameRounds);
+};
 export default brainCalc;
