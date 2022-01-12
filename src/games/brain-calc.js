c6f07df65fb906779a892aa4a1b3f68a5cf536b3
YazykovaDaria <yazykovadasva@gmail.com>
2022-01-12 17:42:10 +0300

    fix format swift

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -12,10 +12,8 @@ const calculate = (number1, number2, operator) => {
   switch (operator) {
     case '+':
       return number1 + number2;
-
     case '-':
       return number1 - number2;
-
     case '*':
       return number1 * number2;
     default:
